66b6f5c018f890fa68902303c45bf3767d3c95e8
unknown <zmr.9841@gmail.com>
2025-01-30 12:18:01 +0530

    Added installation wit userdata

--- Activity-6/install.sh
+++ Activity-6/install.sh
@@ -1,3 +1,5 @@
+# Terraform integrates with cloud-init to configure infrastructure during the initial boot process of a new instance throungh "userdata" input in your instance.
+
 #!/bin/bash
 apt update
 apt install nginx -y
